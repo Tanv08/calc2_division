286b1f762e00309b9b7c86b48319d90c7f0060f7
Tanv08 <tk362@njit.edu>
2021-11-11 16:44:46 -0500

    calc file

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,24 +1,28 @@
 """Testing the Calculator"""
 from calculator.main import Calculator
 
+
 def test_calculator_result():
     """testing calculator result is 0"""
     calc = Calculator()
     assert calc.result == 0
 
+
 def test_calculator_add():
     """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
+    # Arrange by instantiating the calc class
     calc = Calculator()
-    #Act by calling the method to be tested
+    # Act by calling the method to be tested
     calc.add_number(4)
-    #Assert that the results are correct
+    # Assert that the results are correct
     assert calc.result == 4
+
 
 def test_calculator_get_result():
     """Testing the Get result method of the calculator"""
     calc = Calculator()
     assert calc.get_result() == 0
+
 
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
@@ -26,8 +30,16 @@ def test_calculator_subtract():
     calc.subtract_number(1)
     assert calc.get_result() == -1
 
+
+def test_calculator_multiply():
+    """ tests multiplication of two numbers"""
+    calc = Calculator()
+    result = calc.multiply_numbers(1, 2)
+    assert result == 2
+
+
 def test_calculator_divide():
-    """ test Division of two numbers"""
-    calc=Calculator()
-    result=calc.divide_numbers(8,4)
-    assert  result == 2
+    """ tests division of two numbers"""
+    calc = Calculator()
+    result = calc.divide_numbers(8, 4)
+    assert result == 2
